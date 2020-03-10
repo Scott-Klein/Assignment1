385ff72064fa5c32035eb1d59b3bca27bd2e1815
Scott Klein <scotty.klei@gmail.com>
2020-03-10 23:47:03 +1100

    Add project files.

---
+++ Assignment1.tests/Assignment1.tests.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Assignment1/State.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Assignment1tests
+{
+	TEST_CLASS(Assignment1tests)
+	{
+	public:
+		/*
+		Task  1: Create  a  class,  named State,  with  related  data  members  to  store  a 
+		game state (an arrayof nine integers,a 3*3 two-dimensional array of integers, an array 
+		of three vectors, an array of three stacks, or any other data types you prefer to use) 
+		and necessary methodsfor variable initialisation, data input andgame boarddisplay.
+		*/
+		TEST_METHOD(TestMethod1)
+		{
+			State* testState = new State();
+
+			Assert::AreEqual(1, 1);
+			Assert::AreNotEqual(4, 1);
+		}
+
+		TEST_METHOD(ShouldFail)
+		{
+			Assert::IsTrue(false);
+		}
+	};
+}
---
+++ Assignment1/Assignment1.cpp
@@ -0,0 +1,9 @@
+// Assignment1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Assignment 1 --- Data Structures and Algorithms.\n";
+}
